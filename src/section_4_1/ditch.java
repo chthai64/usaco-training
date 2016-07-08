87352ec7609c255267237f284f42a95a97f87b17
chthai64 <chthai64@gmail.com>
2016-07-08 14:49:31 -0400

    Done problem "ditch".

--- src/section_4_1/ditch.java
+++ src/section_4_1/ditch.java
@@ -15,7 +15,7 @@ import java.math.*;
 public class ditch {
 
 	public static void main(String[] args) throws Exception {
-		Input in = fromFile("C:/Users/Chau Thai/Desktop/ditch.in");
+		Input in = fromFile("ditch.in");
 
 		int N = in.nextInt();
 		int M = in.nextInt();
@@ -26,17 +26,15 @@ public class ditch {
 			int to = in.nextInt();
 			int rate = in.nextInt();
 
-			capacity[from][to] = rate;
+			capacity[from][to] += rate;
 		}
 
 		int result = solve(M, capacity);
 
-		//		PrintWriter pw = new PrintWriter(new File("ditch.out"));
-		//		pw.println(result);
-		//		pw.close();
-		//		in.close();
-
-		System.out.println(result);
+		PrintWriter pw = new PrintWriter(new File("ditch.out"));
+		pw.println(result);
+		pw.close();
+		in.close();
 	}
 
 	static int solve(int M, int[][] capacity) {
@@ -44,83 +42,83 @@ public class ditch {
 		return maxFlow.maxFlow();
 	}
 
-		static class MaxFlow {
-			int[][] capacity;
-			int source, sink;
-	
-			public MaxFlow(int[][] capacity, int source, int sink) {
-				this.capacity = capacity;
-				this.source = source;
-				this.sink = sink;
-			}
-	
-			public int maxFlow() {
-				int result = 0;
-	
-				while (true) {
-					// find the capacity of the augmenting path
-					int pathCapacity = findPathBfs();
-	
-					if (pathCapacity > 0) {
-						result += pathCapacity;
-					} else {
-						return result;
-					}
+	static class MaxFlow {
+		int[][] capacity;
+		int source, sink;
+
+		public MaxFlow(int[][] capacity, int source, int sink) {
+			this.capacity = capacity;
+			this.source = source;
+			this.sink = sink;
+		}
+
+		public int maxFlow() {
+			int result = 0;
+
+			while (true) {
+				// find the capacity of the augmenting path
+				int pathCapacity = findPathBfs();
+
+				if (pathCapacity > 0) {
+					result += pathCapacity;
+				} else {
+					return result;
 				}
-			}
-	
-			private int findPathBfs() {
-				Queue<Integer> queue = new LinkedList<Integer>();
-				boolean[] visited = new boolean[capacity.length];
-	
-				int[] from = new int[capacity.length];
-				Arrays.fill(from, -1);
-	
-				queue.add(source);
-				visited[source] = true;
-	
-				while (!queue.isEmpty()) {
-					int where = queue.poll();
-	
-					if (where == sink) {
-						break;
-					}
-	
-					for (int next = 0; next < capacity.length; next++) {
-						if (next != where && !visited[next] && capacity[where][next] > 0) {
-							queue.add(next);
-							visited[next] = true;
-							from[next] = where;
-						}
-					}
-				}
-	
-				// no path found
-				if (from[sink] == -1)
-					return 0;
-	
-				// find capacity of the path
-				int pathCapacity = Integer.MAX_VALUE;
-				int where = sink;
-	
-				while (from[where] != -1) {
-					int prev = from[where];
-					pathCapacity = Math.min(pathCapacity, capacity[prev][where]);
-					where = prev;
-				}
-	
-				// update the residual network
-				where = sink;
-				while (from[where] != -1) {
-					int prev = from[where];
-					capacity[prev][where] -= pathCapacity;
-					capacity[where][prev] += pathCapacity;
-					where = prev;
-				}
-	
-				return pathCapacity;
 			}
 		}
+
+		private int findPathBfs() {
+			Queue<Integer> queue = new LinkedList<Integer>();
+			boolean[] visited = new boolean[capacity.length];
+
+			int[] from = new int[capacity.length];
+			Arrays.fill(from, -1);
+
+			queue.add(source);
+			visited[source] = true;
+
+			while (!queue.isEmpty()) {
+				int where = queue.poll();
+
+				if (where == sink) {
+					break;
+				}
+
+				for (int next = 0; next < capacity.length; next++) {
+					if (next != where && !visited[next] && capacity[where][next] > 0) {
+						queue.add(next);
+						visited[next] = true;
+						from[next] = where;
+					}
+				}
+			}
+
+			// no path found
+			if (from[sink] == -1)
+				return 0;
+
+			// find capacity of the path
+			int pathCapacity = Integer.MAX_VALUE;
+			int where = sink;
+
+			while (from[where] != -1) {
+				int prev = from[where];
+				pathCapacity = Math.min(pathCapacity, capacity[prev][where]);
+				where = prev;
+			}
+
+			// update the residual network
+			where = sink;
+			while (from[where] != -1) {
+				int prev = from[where];
+				capacity[prev][where] -= pathCapacity;
+				capacity[where][prev] += pathCapacity;
+				where = prev;
+			}
+
+			return pathCapacity;
+		}
+	}
 
 	private static Input fromFile(String path) throws IOException {
 		return new Input(new FileInputStream(new File(path)));
