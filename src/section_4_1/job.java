7061c327f523852e983868faa196ab6e2a28c138
chthai64 <chthai64@gmail.com>
2016-07-10 20:06:06 -0400

    done "job" problem.

--- src/section_4_1/job.java
+++ src/section_4_1/job.java
@@ -14,129 +14,71 @@ import java.math.*;
 public class job {
 
 	public static void main(String[] args) throws Exception {
-		Input in = fromFile("/Users/idgmi_dc/Desktop/job.in");
-		
+		Input in = fromFile("job.in");
+
 		int N = in.nextInt();
 		int M1 = in.nextInt();
 		int M2 = in.nextInt();
-		
+
 		int[] A = new int[M1];
 		for (int i = 0; i < A.length; i++) {
 			A[i] = in.nextInt();
 		}
-		
+
 		int[] B = new int[M2];
 		for (int i = 0; i < B.length; i++) {
 			B[i] = in.nextInt();
 		}
 		
-		int result = solve(N, A, B);
+		String result = solve(N, A, B);
+
+		PrintWriter pw = new PrintWriter(new File("job.out"));
+		pw.println(result);
+		pw.close();
+		in.close();
+	}
+
+	static String solve(int N, int[] timeA, int[] timeB) {
+		int[] finishA = new int[timeA.length];
+		int[] finishB = new int[timeB.length];
 		
-		System.out.println(result);
+		int[] jobA = new int[N];
+		int[] jobB = new int[N];
+		
+		process(jobA, timeA, finishA);
+		process(jobB, timeB, finishB);
+		
+		int t1 = 0;
+		for (int i : finishA) {
+			t1 = Math.max(t1, i);
+		}
+		
+		int t = 0;
+		for (int i = 0; i < N; i++) {
+			t = Math.max(t, jobA[i] + jobB[N - 1 - i]);
+		}
+		
+		return t1 + " " + t;
 	}
 	
-	static int solve(int N, int[] A, int[] B) {
-		// compute first layer max rate
-		int[][] graph1 = new int[2 + N + A.length][2 + N + A.length];
-		
-		// match jobs to machines A and source to jobs.
-		for (int job = 0; job < N; job++) {
-			for (int machineA = 0; machineA < A.length; machineA++) {
-				graph1[job + 1][N + 1 + machineA] = 1;
+	private static void process(int[] job, int[] time, int[] finish) {
+		for (int i = 0; i < job.length; i++) {
+			int minTime = Integer.MAX_VALUE;
+			int minIndex = 0;
+			
+			for (int j = 0; j < finish.length; j++) {
+				int t = finish[j] + time[j];
+				if (t < minTime) {
+					minIndex = j;
+					minTime = finish[j] + time[j];
+				}
 			}
 			
-			graph1[0][job + 1] = 1;
+			finish[minIndex] = minTime;
+			job[i] = minTime;
 		}
-		
-		// match machines A to immediate sink
-		for (int machineA = 0; machineA < A.length; machineA++) {
-			graph1[1 + N + machineA][graph1.length - 1] = A[machineA];
-		}
-		
-		int rate1 = new MaxFlow(graph1, 0, graph1.length - 1).maxFlow();
-		System.out.println("rate1: " + rate1);
-		
-		return 0;
 	}
 	
-	static class MaxFlow {
-		int[][] capacity;
-		int source, sink;
-
-		public MaxFlow(int[][] capacity, int source, int sink) {
-			this.capacity = capacity;
-			this.source = source;
-			this.sink = sink;
-		}
-
-		public int maxFlow() {
-			int result = 0;
-
-			while (true) {
-				// find the capacity of the augmenting path
-				int pathCapacity = findPathBfs();
-
-				if (pathCapacity > 0) {
-					result += pathCapacity;
-				} else {
-					return result;
-				}
-			}
-		}
-
-		private int findPathBfs() {
-			Queue<Integer> queue = new LinkedList<Integer>();
-			boolean[] visited = new boolean[capacity.length];
-
-			int[] from = new int[capacity.length];
-			Arrays.fill(from, -1);
-
-			queue.add(source);
-			visited[source] = true;
-
-			while (!queue.isEmpty()) {
-				int where = queue.poll();
-
-				if (where == sink) {
-					break;
-				}
-
-				for (int next = 0; next < capacity.length; next++) {
-					if (next != where && !visited[next] && capacity[where][next] > 0) {
-						queue.add(next);
-						visited[next] = true;
-						from[next] = where;
-					}
-				}
-			}
-
-			// no path found
-			if (from[sink] == -1)
-				return 0;
-
-			// find capacity of the path
-			int pathCapacity = Integer.MAX_VALUE;
-			int where = sink;
-
-			while (from[where] != -1) {
-				int prev = from[where];
-				pathCapacity = Math.min(pathCapacity, capacity[prev][where]);
-				where = prev;
-			}
-
-			// update the residual network
-			where = sink;
-			while (from[where] != -1) {
-				int prev = from[where];
-				capacity[prev][where] -= pathCapacity;
-				capacity[where][prev] += pathCapacity;
-				where = prev;
-			}
-
-			return pathCapacity;
-		}
-	}
-
 	private static Input fromFile(String path) throws IOException {
 		return new Input(new FileInputStream(new File(path)));
 	}
